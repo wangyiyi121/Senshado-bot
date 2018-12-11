601de92774b491a108890aba3cea85c4052a76a9
wangyiyi121 <38238570+wangyiyi121@users.noreply.github.com>
2018-12-11 19:46:41 -0400

    Add files via upload

---
+++ senshado_bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+
+Client=discord.Client()
+client=commands.Bot(command_prefix="")
+
+@client.event
+@asyncio.coroutine
+def on_ready():
+    print("Hello World!")
+
+@client.event
+@asyncio.coroutine
+def on_message(message):
+    if message.channel.id=="522188850332696630":
+        l=message.content
+        for i in range(len(l)):
+            if l[i:i+4]=="http":
+                if l.count("/")!=0:
+                    yield from client.send_message(message.author,"Hey there! Please don't send links in the general channel. Your message was auto deleted by the bot made by the Sensha-do Federation.\nHere's your message if you wish to edit it:\n%s" % message.content)
+                    yield from client.delete_message(message)
+
+                
+client.run("NTIyMTU0MDM3MjU3MTc1MDQx.DvG2cA.RTXnK_U2Yzh48ImJU9lzYxcc3lc")
