6e03aff69b5adb6eb4861717cb149123ee6f6184
wangyiyi121 <38238570+wangyiyi121@users.noreply.github.com>
2018-12-11 20:11:02 -0400

    Update senshado_bot.py

--- senshado_bot.py
+++ senshado_bot.py
@@ -21,6 +21,7 @@ def on_message(message):
                 if l.count("/")!=0:
                     yield from client.send_message(message.author,"Hey there! Please don't send links in the general channel. Your message was auto deleted by the bot made by the Sensha-do Federation.\nHere's your message if you wish to edit it:\n%s" % message.content)
                     yield from client.delete_message(message)
+                    break
 
                 
 client.run("NTIyMTU0MDM3MjU3MTc1MDQx.DvG2cA.RTXnK_U2Yzh48ImJU9lzYxcc3lc")
