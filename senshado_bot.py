2439ae9429bfccc5b57250bd7b4ca498507615bd
wangyiyi121 <38238570+wangyiyi121@users.noreply.github.com>
2019-06-30 17:39:01 -0300

    Update senshado_bot.py

--- senshado_bot.py
+++ senshado_bot.py
@@ -18,13 +18,14 @@ def on_ready():
 @asyncio.coroutine
 def on_message(message):
     if message.channel.id=="493819751529840652":
-        l=message.content
+        l=message.content.lower()
         for i in range(len(l)):
             if l[i:i+4]=="http":
                 if l.count("/")!=0:
-                    yield from client.send_message(message.author,"Hey there! Please don't send links in the general channel. Your message was auto deleted by the bot made by the Sensha-do Federation.\nHere's your message if you wish to edit it:\n%s" % message.content)
-                    yield from client.delete_message(message)
-                    break
+                    if not(("jpg" in l) or ("jpeg" in l) or ("png" in l) or ("gif" in l)):
+                        yield from client.send_message(message.author,"Hey there! Please don't send links in the general channel. Your message was auto deleted by the bot made by the Sensha-do Federation.\nHere's your message if you wish to edit it:\n%s" % message.content)
+                        yield from client.delete_message(message)
+                        break
                     
     if message.channel.id=="496541228771573770":
         if message.content.startswith("-botstatus"):
